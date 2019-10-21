a67987f9844e167aef331b0434d78f092b77c5ac
Shaan Patel <sbp45@case.edu>
2019-10-21 00:51:34 -0400

    working login, logout, create account, delete account, update account info; redirect to proper templates

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 
+from bson.objectid import ObjectId
 from flask import (Flask, flash, redirect, render_template, request, session,
                    url_for)
 from pymongo import MongoClient
@@ -8,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 client = MongoClient("mongodb+srv://hlabs_1:thinkBox@aiml-thzu0.mongodb.net/test?retryWrites=true&w=majority")
 
-# Starting page is the login page
+# User can login to their account
 @app.route('/', methods=['GET', 'POST'])
 def start():
     if request.method == "POST":
@@ -27,7 +28,6 @@ def start():
     return render_template('start.html', template_error="")
 
 # User can create an account
-# Once done, they will be redirected to home page or asked to reenter info
 @app.route('/createaccount', methods=['GET', 'POST'])
 def createaccount():
     db = client['aiML']
@@ -52,12 +52,34 @@ def createaccount():
             return render_template('createaccount.html', template_error = "Could not create account: password fields do not match")
     return render_template('createaccount.html', template_error = "")
 
-# Main page containing ai.ML functionality
+# Main page containing ai.ML functionality TODO
 @app.route('/main', methods=['GET', 'POST'])
 def main():
     if not 'id' in session:
         return redirect(url_for('start'))
     return render_template('main.html')
+
+# User can logout from their account
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if not 'id' in session:
+        return redirect(url_for('start'))
+    if "returnhome" in request.form:
+        if request.form["returnhome"] == "Yes":
+            session.pop("username", None)
+            session.pop("email", None)
+            session.pop("id", None)
+            return redirect(url_for('start'))
+        if request.form["returnhome"] == "No":
+            return redirect(url_for('main'))
+    return render_template('logout.html')
+
+# User can view basic profile information, update email, username or password, or delete account
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if not 'id' in session:
+        return redirect(url_for('start'))
+    return render_template('profile.html', profile = session)
 
 # User can update their email
 @app.route('/updateemail', methods=['GET', 'POST'])
@@ -68,12 +90,12 @@ def update_email():
         return redirect(url_for('start'))
     if request.method == "GET":
         user_info = collection.find_one({"email":f"{session['email']}"})
-        email = user_info['email']
+       # email = user_info['email']
     if request.method == "POST":
         new_email = request.form['new-email']
         if collection.count_documents({"email":f"{request.form['new-email']}"}) > 0:
             return render_template('updateemail.html', template_error = "Could not update email: email is already a part of another account", profile = session)
-        collection.update_one({"id":f"{session['id']}"}, {"$set":{"email":f"{new_email}"}})
+        collection.update_one({"_id":ObjectId(f"{session['id']}")}, {"$set":{"email":f"{new_email}"}})
         session['email'] = new_email
         client.close()
         return redirect(url_for('profile'))
@@ -93,7 +115,7 @@ def update_username():
         new_username = request.form['new-username']
         if collection.count_documents({"username":f"{request.form['new-username']}"}) > 0:
             return render_template('updateusername.html', template_error = "Could not update username: username is already a part of another account", profile = session)
-        collection.update_one({"id":f"{session['id']}"}, {"$set":{"username":f"{new_username}"}})
+        collection.update_one({"_id":ObjectId(f"{session['id']}")}, {"$set":{"username":f"{new_username}"}})
         session['username'] = new_username
         client.close()
         return redirect(url_for('profile'))
@@ -110,7 +132,7 @@ def update_password():
         if request.form['new-password'] == request.form['retype-new-password']:
             if collection.count_documents({"password":f"{request.form['old-password']}"}) == 1:
                 new_password = request.form['new-password']
-                collection.update_one({"id":f"{session['id']}"}, {"$set":{"password":f"{new_password}"}})
+                collection.update_one({"_id":ObjectId(f"{session['id']}")}, {"$set":{"password":f"{new_password}"}})
                 session['password'] = new_password
                 client.close()
                 return redirect(url_for('profile'))
@@ -119,3 +141,18 @@ def update_password():
         else:
             return render_template('updatepassword.html', template_error = "Could not change password: New password fields do not match")
     return render_template('updatepassword.html', template_error = "")
+
+# User can delete an account
+@app.route('/deleteaccount', methods=['GET', 'POST'])
+def delete_account():
+    db = client['aiML']
+    collection = db['users']
+    if not 'id' in session:
+        return redirect(url_for('start'))
+    if request.method == "POST":
+        if "delete-btn" in request.form:
+            collection.delete_one({"_id":ObjectId(f"{session['id']}")})
+            return redirect(url_for('start'))
+        if "returnhome" in request.form:
+            return redirect(url_for('main'))
+    return render_template('deleteaccount.html')
