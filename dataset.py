b70ba25f7d99e9f8d838d6f9b2308e36ae1b3a76
rtatton <rdt17@protonmail.com>
2019-10-29 19:30:54 -0400

    Dataset v0.1: Some Tweaks and More Feature and Dataset Functionality
    - Added type() property to Element and Feature that returns either "numerical" or "categorical."
    - Updated Feature default label parameter value to ' ' and moved after elements parameter to allow it not to be specified. More on this change later.
    - Added deleted attribute to Feature which is a boolean flag.
    - Added values property to Feature to allow only the values of its Elements to be returned as a list. This will make it easier to use existing  computation libraries like numpy and pandas.
    - Added add() method to Feature that takes an Element and adds it to the Feature if its type agrees with those in the Feature. This check is done to constrain a Feature to only one type and not be mixed. A TypeError is raised if its type is not the same as the Feature.
    - Added remove() method to Feature that takes an Element and removes it if it exists. Raises a ValueError if the Element is not in the Feature.
    - Added delete() and restore() methods that change the attribute value deleted.
    - Added update() method that replaces an Element with another Element. Raises ValueError if the Element to replace could not be found.
    - Added count() method that returns the number of times an Element appears in the Feature. Based on value of the Element.
    - Changed Dataset.features to List. However, this has made lookup functions quite cumbersone. In order to keep it as a Dict, each Feature must be guaranteed to have a unique label as this is the key to the key value pair. We could use the built-in id() function which is guaranteed to be unique. The user could override this default in the case a label is defined but a check would need to be performed to make sure that the user label does not already exist. If the list is kept then it is better to make a utility function (something like get_feature_index()) so that the syntax is less verbose.
    - Added n_numerical() and n_categorical() methods which are based on n_features().

--- dataset.py
+++ dataset.py
@@ -9,6 +9,10 @@ class Element():
         self.deleted = deleted
 
     @property
+    def type(self) -> str:
+        return 'numerical' if self.is_numeric() else 'categorical'
+
+    @property
     def missing(self) -> bool:
         return self.value in {None, ''}
 
@@ -21,15 +25,18 @@ class Element():
     def is_numeric(self) -> bool:
         if isinstance(self.value, str):
             return str.isnumeric(self.value)
-        else:
+        elif isinstance(self.value, int) or isinstance(self.value, float):
             return True
+        else:
+            return False
 
     def __repr__(self):
         name = self.__class__.__name__
         v = self.value
+        t = self.type
         d = self.deleted
         m = self.missing
-        return f'{name}(value={v}, missing={m}, deleted={d})'
+        return f'{name}(value={v}, type={t}, missing={m}, deleted={d})'
 
     def __eq__(self, e):
         if isinstance(e, self.__class__):
@@ -46,19 +53,57 @@ class Element():
     def __ge__(self, e):
         return self.value >= e.value
 
+    def __le__(self, e):
+        return self.value <= e.value
+
 
 class Feature():
 
-    def __init__(self, label: str, elements: List[Element] = None):
-        self.label = label
+    def __init__(self, elements: List[Element] = None, label: str = '', deleted=False):
         if not elements:
             elements = []
         self.elements = elements
+        self.label = label
+        self.deleted = deleted
 
     @property
-    def values(self):
+    def type(self) -> str:
+        if all([e.is_numeric() for e in self.elements]):
+            return 'numerical'
+        else:
+            return 'categorical'
+
+    @property
+    def values(self) -> List:
         return [e.value for e in self.elements]
 
+    def add(self, e: Element):
+        if e.type == self.type:
+            self.elements.append(e)
+        else:
+            raise TypeError('Element must be the same type as the feature.')
+
+    def remove(self, e: Element):
+        try:
+            self.elements.remove(e)
+        except ValueError:
+            print('Element not found in feature.')
+
+    def delete(self):
+        self.deleted = True
+
+    def restore(self):
+        self.deleted = False
+
+    def update(self, e_old: Element, e_new: Element):
+        try:
+            self.elements[self.elements.index(e_old)] = e_new
+        except ValueError:
+            print('Element not found in feature.')
+
+    def count(self, e: Element):
+        return self.elements.count(e)
+  
     def n_elements(self) -> int:
         return len(self.elements)
 
@@ -70,44 +115,61 @@ class Feature():
         l = self.label
         n = len(self.elements)
         t = 'numerical' if self.is_numeric() else 'categorical'
-        return f'{name}(label="{l}", n_elements={n}, type={t})'
+        d = self.deleted
+        return f'{name}(label="{l}", n_elements={n}, type={t}, deleted={d})'
 
     def __eq__(self, f):
         if isinstance(f, self.__class__):
             if self.n_elements() == f.n_elements():
-                return
-                all([so == fo for so, fo in zip(self.elements, f.elements)])
+                comps = [so == fo for so, fo in zip(self.elements, f.elements)]
+                return all(comps)
         else:
             return False
 
 
 class Dataset():
 
-    def __init__(self, dataset_id: str, data: List[Feature] = None, **metadata):
+    def __init__(self, data: List[Feature] = None, dataset_id: str = None, **metadata):
         self.dataset_id = dataset_id
         if not data:
             data = []
-        self.features = {f.label: f for f in data}
+        self.features = data
         self.metadata = metadata
 
     @property
     def values(self):
         return {
             f.label:
-            [o.value for o in f.elements] for f in self.features.values()
+            [o.value for o in f.elements] for f in self.features.values
             }
+
+    def add(self, f: Feature):
+        self.features.append(f)
+
+    def delete(self, f: Feature):
+        self.features[self.features.index(f)].delete()
+
+    def restore(self, f: Feature):
+        self.features[self.features.index(f)].restore()
+
+    def n_features(self) -> int:
+        n_deleted = len([f for f in self.features if f.deleted])
+        return len(self.features) - n_deleted
+
+    def n_numerical(self):
+        return len([f.is_numeric() for f in self.features])
+
+    def n_categorical(self):
+        return self.n_features() - self.n_numerical()
+
+    def n_elements(self, f: Feature) -> int:
+        return self.features[self.features.index(f)].n_elements()
+
+    def is_numeric(self, f: Feature) -> bool:
+        return self.features[self.features.index(f)].is_numeric()
 
     def __repr__(self):
         name = self.__class__.__name__
         ds_id = self.dataset_id
         n = self.n_features()
         return f'{name}(id={ds_id}, n_features={n})'
-
-    def n_features(self) -> int:
-        return len(self.features.keys())
-
-    def n_elements(self, feature: str) -> int:
-        return self.features[feature].n_elements()
-
-    def is_numeric(self, feature: str) -> bool:
-        return self.features[feature].is_numeric()
