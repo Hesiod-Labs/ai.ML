c42ed16ae099181b7869ea773c7f29c106d14430
rtatton <rdt17@protonmail.com>
2019-11-16 14:20:05 -0500

    Changed static variables to be a dictionary of parameters.
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- ml_parameters.py
+++ ml_parameters.py
@@ -1,49 +1,54 @@
-LOGISTIC_REGRESSION = (
-    'penalty',
-    'dual',
-    'tol',
-    'C',
-    'fit_intercept',
-    'intercept_scaling',
-    'max_iter',
-    'l1_ratio'
-)
 
-SUPPORT_VECTOR_CLASSIFICATION = (
-    'C',
-    'kernel',
-    'degree',
-    'gamma',
-    'coeff0',
-    'shrinking',
-    'tol',
-    'max_iter',
-)
 
-K_NEAREST_NEIGHBORS = (
-    'n_neighbors',
-    'weights',
-    'algorithm',
-    'leaf_size',
-    'p',
-)
-
-DECISION_TREE_CLASSIFICATION = (
-    'criterion',
-    'splitter',
-    'max_depth',
-    'min_samples_split',
-    'min_samples_leaf',
-    'min_weight_fraction_leaf',
-    'max_features',
-    'max_leaf_nodes',
-    'min_impurity_decrease',
-)
-
-LINEAR_DISCRIMINANT_ANALYSIS = (
-    'solver',
-    'shrinkage',
-    'priors',
-    'n_components',
-    'tol'
-)
+PARAMETERS = {
+    'logistic regression':
+        (
+            'penalty',
+            'dual',
+            'tol',
+            'C',
+            'fit_intercept',
+            'intercept_scaling',
+            'max_iter',
+            'l1_ratio'
+        ),
+    'support vector classification':
+        (
+            'C',
+            'kernel',
+            'degree',
+            'gamma',
+            'coeff0',
+            'shrinking',
+            'tol',
+            'max_iter',
+        ),
+    'k nearest neighbors':
+        (
+            'n_neighbors',
+            'weights',
+            'algorithm',
+            'leaf_size',
+            'p',
+        ),
+    'decision tree classification':
+        (
+            'criterion',
+            'splitter',
+            'max_depth',
+            'min_samples_split',
+            'min_samples_leaf',
+            'min_weight_fraction_leaf',
+            'max_features',
+            'max_leaf_nodes',
+            'min_impurity_decrease',
+        ),
+    'linear discriminant analysis':
+        (
+            'solver',
+            'shrinkage',
+            'priors',
+            'n_components',
+            'tol'
+        )
+}
