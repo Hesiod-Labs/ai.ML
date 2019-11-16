1ba890471760d9c903a37e344afb8f908f3791cb
rtatton <rdt17@protonmail.com>
2019-11-16 13:55:46 -0500

    scikit-learn parameters to be implemented and accessed by the Dash frontend when assembling the user interface.
    Signed-off-by: rtatton <rdt17@protonmail.com>

---
+++ ml_parameters.py
@@ -0,0 +1,49 @@
+LOGISTIC_REGRESSION = (
+    'penalty',
+    'dual',
+    'tol',
+    'C',
+    'fit_intercept',
+    'intercept_scaling',
+    'max_iter',
+    'l1_ratio'
+)
+
+SUPPORT_VECTOR_CLASSIFICATION = (
+    'C',
+    'kernel',
+    'degree',
+    'gamma',
+    'coeff0',
+    'shrinking',
+    'tol',
+    'max_iter',
+)
+
+K_NEAREST_NEIGHBORS = (
+    'n_neighbors',
+    'weights',
+    'algorithm',
+    'leaf_size',
+    'p',
+)
+
+DECISION_TREE_CLASSIFICATION = (
+    'criterion',
+    'splitter',
+    'max_depth',
+    'min_samples_split',
+    'min_samples_leaf',
+    'min_weight_fraction_leaf',
+    'max_features',
+    'max_leaf_nodes',
+    'min_impurity_decrease',
+)
+
+LINEAR_DISCRIMINANT_ANALYSIS = (
+    'solver',
+    'shrinkage',
+    'priors',
+    'n_components',
+    'tol'
+)
